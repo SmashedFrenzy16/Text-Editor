1246726fc4cf3f3d5ed8f2a144af14a1c2510526
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-06-25 16:49:22 +0100

    Update text-editor.py

--- text-editor.py
+++ text-editor.py
@@ -1,1 +1,17 @@
-                                                                                              
+from tkinter import *
+from tkinter.filedialog import asksaveasfilename
+
+root = Tk()
+rooot.title("WordEdit")
+root.geometry("600x600")
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
