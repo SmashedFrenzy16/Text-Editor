445c0d83f2561daf893f3f016396eed728ca27c7
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-06-25 21:58:14 +0100

    Update text-editor.py

--- text-editor.py
+++ text-editor.py
@@ -11,7 +11,24 @@ scrollbar = Scrollbar(root).pack(side = RIGHT, fill = Y)
 
 editor = Text(root, width = 400, height = 450, yscrollcommand = scrollbar.set).pack(fill = BOTH)
 
+scrollbar.config(command = editor.yview)
 
+buttonSave = Button(root, text = 'Save', font = ('normal', 10), command = save, bg='purple')
+
+def save():
+  
+  filepath = asksaveasfilename(defaultextension = "txt", filetypes = [("Text Files", "*.txt"), ("All Files", "*.*")])
+  
+  if not filepath:
+    
+    return
+  
+    with open(filepath, "w") as output_file:
+      
+      text = editor.get(1.0, tkinter.END)
+      output_file.write(text)
+   root.title(f"Title - {filepath}")
+  
 
 
 root.mainloop()
